2c92cceca582a9dc848ed950e1021dc9f245c16a
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-07-10 18:45:56 -0500

    Base composition

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -1,14 +1,42 @@
-import React from 'react';
-import { TodoProvider } from '../TodoContext';
-import { AppUI } from './AppUI';
-
+import React, { useState } from 'react';
 
 function App() {
+  const [state, setstate] = useState('Sharing state');
   return (
-    <TodoProvider>
-      <AppUI />
-    </TodoProvider>
+    <React.Fragment>
+      <TodoHeader>
+        <TodoCounter />
+        <TodoSearch />
+      </TodoHeader>
+
+      <TodoList>
+        <TodoItem state={state} />
+      </TodoList>
+    </React.Fragment>
   );
 }
+function TodoHeader({ children }) {
+  return <header>{children}</header>;
+}
+function TodoList({ children }) {
+  return <section className="TodoList-container">{children}</section>;
+}
+function TodoCounter() {
+  return <p>TodoCounter</p>;
+}
+function TodoSearch() {
+  return <p>TodoSearch</p>;
+}
+function TodoItem({ state }) {
+  return <p>TodoItem: {state}</p>;
+}
+
+// function App() {
+//   return (
+//     <TodoProvider>
+//       <AppUI />
+//     </TodoProvider>
+//   );
+// }
 
 export default App;
