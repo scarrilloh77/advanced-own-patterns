edfcc9d1798904eef860f0e33de21a3a36e61add
Sebastian Carrillo <scarrilloh77@gmail.com>
2023-07-11 16:30:51 -0500

    Base HOC

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
+function App({ saludo, name }) {
+  return (
+    <h1>
+      {saludo}, {name}!
+    </h1>
+  );
+}
+
+function WithSaludo(WrappedComponent) {
+  return function WrappedComponentWithSaludo(saludo) {
+    return function RealComponent(props) {
+      return (
+        <Fragment>
+          <WrappedComponent {...props} saludo={saludo} />
+          <p>Estamos acompañando al WrappedComponent</p>
+        </Fragment>
+      );
+    };
+  };
+}
+
+const AppWithSaludo = WithSaludo(App)('Que pasa');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppWithSaludo name="Sebas" />
   </React.StrictMode>
 );
